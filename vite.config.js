6b077a914dc597be0c64d905103009027975f93e
iconrev <iconserver6@gmail.com>
2025-08-18 22:48:25 -0300

    vite_config

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,18 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  server: {
+    host: true, // Permite que o servidor seja acessível externamente
+  },
+  preview: {
+    host: true, // Permite que o servidor de preview seja acessível externamente
   },
 })
